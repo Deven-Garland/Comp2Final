2b6a7c0150328df123596578471171336a8f057c
Deven-Garland <tus21678@temple.edu>
2026-04-17 19:43:34 -0400

    Added all of my previous from lab 6 ( server, datastructurs, etc) Also added skelton files that need to be worked on.  (other data structures, client, platform server)

--- arcade_project/client.py
+++ arcade_project/client.py
@@ -1,15 +1,1 @@
-"""
-client.py
-Main entry point for the arcade client.
-Run this file to launch the Pygame arcade interface.
-"""
-from client.arcade_client import ArcadeClient
-
-
-def main():
-    client = ArcadeClient()
-    client.run()
-
-
-if __name__ == "__main__":
-    main()
+# Run client (you implement).
