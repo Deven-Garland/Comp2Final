9e283ab447072165863364bbb82e88838d7bcb84
Vraj H. Patel <vrajp9451@gmail.com>
2026-04-17 21:43:41 -0400

    Add Edge class to graph.py

--- arcade_project/datastructures/graph.py
+++ arcade_project/datastructures/graph.py
@@ -1,1 +1,8 @@
 # Graph (final project — you implement).
+from datastructures.hash_table import HashTable
+from datastructures.array import ArrayList
+
+class Edge:
+  def __init__(elf, destination, weight=1):
+    self.destination = destination
+    self.weight = weight
