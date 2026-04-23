25c06389ae09d2f7015429de6e5d19be540db3d9
Deven-Garland <tus21678@temple.edu>
2026-04-23 17:54:57 -0400

    C++ server imported from lab 7

--- arcade_project/cpp_server/src/json_serializer.cpp
+++ arcade_project/cpp_server/src/json_serializer.cpp
@@ -1,42 +1,37 @@
 /*
 json_serializer.cpp - JSON serialization implementation
 
-Author: [Student Name]
-Date: [Date]
+Author: [Deven Garland]
+Date: [2/1/2026]
 */
 
 #include "json_serializer.h"
 
+#include <sstream>
+#include <string>
+
 std::string JSONSerializer::serialize(const Player& player) {
-    std::ostringstream ss;
-    ss << "{"
-       << "\"id\":" << player.get_id() << ","
-       << "\"name\":\"" << player.get_name() << "\","
-       << "\"x\":" << player.get_x() << ","
-       << "\"y\":" << player.get_y() << ","
-       << "\"socket\":" << player.get_socket() << ","
-       << "\"character_type\":\"" << player.get_character_type() << "\","
-       << "\"status\":\"" << player.get_status() << "\""
-       << "}";
-    return ss.str();
+    std::ostringstream oss;
+
+    oss << "{";
+    oss << "\"id\":" << player.get_id();
+    oss << ",\"name\":\"" << player.get_name() << "\"";
+    oss << ",\"x\":" << player.get_x();
+    oss << ",\"y\":" << player.get_y();
+    oss << ",\"socket\":" << player.get_socket();
+    oss << "}";
+
+    return oss.str();
 }
 
 Player JSONSerializer::deserialize(const std::string& data) {
-    // Extract fields using helper functions
     int id = extractInt(data, "id");
     std::string name = extractString(data, "name");
     float x = extractFloat(data, "x");
     float y = extractFloat(data, "y");
     int socket = extractInt(data, "socket");
-    std::string character_type = extractString(data, "character_type");
-    std::string status = extractString(data, "status");
-    
-    // Create player and set additional fields
-    Player player(id, name, x, y, socket);
-    player.set_character_type(character_type);
-    player.set_status(status);
-    
-    return player;
+
+    return Player(id, name, x, y, socket);
 }
 
 std::string JSONSerializer::getName() const {
@@ -45,38 +40,47 @@ std::string JSONSerializer::getName() const {
 
 // Helper function to extract integer from JSON
 int JSONSerializer::extractInt(const std::string& json, const std::string& key) {
-    std::string search = "\"" + key + "\":";
-    size_t pos = json.find(search);
-    if (pos == std::string::npos) return 0;
-    
-    pos += search.length();
-    size_t end = json.find_first_of(",}", pos);
-    std::string value = json.substr(pos, end - pos);
-    
+    std::string searchKey = "\"" + key + "\":";
+    size_t keyPos = json.find(searchKey);
+
+    if (keyPos == std::string::npos) {
+        return 0;
+    }
+
+    size_t valueStart = keyPos + searchKey.length();
+    size_t valueEnd = json.find_first_of(",}", valueStart);
+
+    std::string value = json.substr(valueStart, valueEnd - valueStart);
     return std::stoi(value);
 }
 
 // Helper function to extract float from JSON
 float JSONSerializer::extractFloat(const std::string& json, const std::string& key) {
-    std::string search = "\"" + key + "\":";
-    size_t pos = json.find(search);
-    if (pos == std::string::npos) return 0.0f;
-    
-    pos += search.length();
-    size_t end = json.find_first_of(",}", pos);
-    std::string value = json.substr(pos, end - pos);
-    
+    std::string searchKey = "\"" + key + "\":";
+    size_t keyPos = json.find(searchKey);
+
+    if (keyPos == std::string::npos) {
+        return 0.0f;
+    }
+
+    size_t valueStart = keyPos + searchKey.length();
+    size_t valueEnd = json.find_first_of(",}", valueStart);
+
+    std::string value = json.substr(valueStart, valueEnd - valueStart);
     return std::stof(value);
 }
 
 // Helper function to extract string from JSON
 std::string JSONSerializer::extractString(const std::string& json, const std::string& key) {
-    std::string search = "\"" + key + "\":\"";
-    size_t pos = json.find(search);
-    if (pos == std::string::npos) return "";
-    
-    pos += search.length();
-    size_t end = json.find("\"", pos);
-    
-    return json.substr(pos, end - pos);
+    std::string searchKey = "\"" + key + "\":\"";
+    size_t keyPos = json.find(searchKey);
+
+    if (keyPos == std::string::npos) {
+        return "";
+    }
+
+    size_t valueStart = keyPos + searchKey.length();
+    size_t valueEnd = json.find("\"", valueStart);
+
+    return json.substr(valueStart, valueEnd - valueStart);
 }
--- arcade_project/cpp_server/src/text_serializer.cpp
+++ arcade_project/cpp_server/src/text_serializer.cpp
@@ -1,51 +1,46 @@
 /*
 text_serializer.cpp - Text serialization implementation
 
-Author: [Student Name]
-Date: [Date]
+Author: [Deven Garland]
+Date: [1/28/2026]
 */
 
 #include "text_serializer.h"
 
+#include <sstream>
+#include <string>
+
 std::string TextSerializer::serialize(const Player& player) {
-    std::ostringstream ss;
-    ss << player.get_id() << "|"
-       << player.get_name() << "|"
-       << player.get_x() << "|"
-       << player.get_y() << "|"
-       << player.get_socket() << "|"
-       << player.get_character_type() << "|"
-       << player.get_status();
-    return ss.str();
+    std::ostringstream oss;
+
+    oss << player.get_id() << "|"
+        << player.get_name() << "|"
+        << player.get_x() << "|"
+        << player.get_y() << "|"
+        << player.get_socket();
+
+    return oss.str();
 }
 
 Player TextSerializer::deserialize(const std::string& data) {
-    std::istringstream ss(data);
-    std::string id_str, name, x_str, y_str, socket_str, character_type, status;
-    
-    // Parse fields separated by |
-    std::getline(ss, id_str, '|');
-    std::getline(ss, name, '|');
-    std::getline(ss, x_str, '|');
-    std::getline(ss, y_str, '|');
-    std::getline(ss, socket_str, '|');
-    std::getline(ss, character_type, '|');
-    std::getline(ss, status, '|');
-    
-    // Convert strings to appropriate types
+    std::istringstream iss(data);
+
+    std::string id_str, name, x_str, y_str, socket_str;
+
+    std::getline(iss, id_str, '|');
+    std::getline(iss, name, '|');
+    std::getline(iss, x_str, '|');
+    std::getline(iss, y_str, '|');
+    std::getline(iss, socket_str);
+
     int id = std::stoi(id_str);
+    int socket = std::stoi(socket_str);
     float x = std::stof(x_str);
     float y = std::stof(y_str);
-    int socket = std::stoi(socket_str);
-    
-    // Create player and set additional fields
-    Player player(id, name, x, y, socket);
-    player.set_character_type(character_type);
-    player.set_status(status);
-    
-    return player;
+
+    return Player(id, name, x, y, socket);
 }
 
 std::string TextSerializer::getName() const {
-    return "Text";
+    return "TextSerializer";
 }
