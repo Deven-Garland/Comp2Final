212abe2e35fc9639b70324fd2428cec6bbead2dc
Ellie <elliegrace2150@gmail.com>
2026-04-23 19:33:43 -0400

    Restore persistent accounts with BloomFilter

--- arcade_project/platform_server/accounts.py
+++ arcade_project/platform_server/accounts.py
@@ -15,29 +15,61 @@ Author: Mennah Khaled Dewidar
 Date: [4/18/2026]
 Lab: Final Project - Algorithm One
 """
+
+import json
+import os
+
 from datastructures.hash_table import HashTable
 from datastructures.bloom_filter import BloomFilter
- 
+
+# Where accounts are saved on disk
+ACCOUNTS_FILE = os.path.join(os.path.dirname(__file__), "accounts_data.json")
+
+
 class Account:
     def __init__(self, username, password):
         self.username = username
         self.password = password
- 
+
     def __str__(self):
         return f"Account({self.username})"
- 
+
     def __repr__(self):
-        """
-        returns a developer-friendly string representation (often the same as __str__ for simple classes), used in the interactive shell
-        """
         return self.__str__()
- 
- 
+
+
 class Accounts:
     def __init__(self):
         self.accounts = HashTable()
         self.username_filter = BloomFilter()
- 
+        self._load()
+
+    def _load(self):
+        """Load accounts from disk if the file exists."""
+        if not os.path.exists(ACCOUNTS_FILE):
+            return
+        try:
+            with open(ACCOUNTS_FILE, "r") as f:
+                data = json.load(f)
+            for username, password in data.items():
+                account = Account(username, password)
+                self.accounts[username] = account
+                self.username_filter.add(username)
+        except Exception as e:
+            print(f"[accounts] Could not load accounts file: {e}")
+
+    def _save(self):
+        """Save all accounts to disk as a JSON file."""
+        try:
+            data = {}
+            for key in self.accounts:
+                account = self.accounts[key]
+                data[account.username] = account.password
+            with open(ACCOUNTS_FILE, "w") as f:
+                json.dump(data, f, indent=2)
+        except Exception as e:
+            print(f"[accounts] Could not save accounts file: {e}")
+
     def register(self, username, password):
         if self.username_filter.contains(username):
             if username in self.accounts:
@@ -45,27 +77,29 @@ class Accounts:
         account = Account(username, password)
         self.accounts[username] = account
         self.username_filter.add(username)
+        self._save()
         return True
- 
+
     def login(self, username, password):
         if username not in self.accounts:
             return False
         account = self.accounts[username]
         return account.password == password
- 
+
     def get_account(self, username):
         if username not in self.accounts:
             return None
         return self.accounts[username]
- 
+
     def exists(self, username):
         return username in self.accounts
- 
+
     def remove(self, username):
         if username not in self.accounts:
             return False
         self.accounts.remove(username)
+        self._save()
         return True
- 
+
     def __len__(self):
         return len(self.accounts)
